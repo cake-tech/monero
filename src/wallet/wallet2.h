a6c432ba12a5e9c1da378ab5afae456d4cccd836
OmarHatem <omarh.ismail1@gmail.com>
2024-03-14 16:44:39 +0200

    remove duplicate declaration

--- src/wallet/wallet2.h
+++ src/wallet/wallet2.h
@@ -1717,8 +1717,6 @@ private:
     
     boost::shared_mutex m_transfers_mutex;
 
-    boost::shared_mutex m_transfers_mutex;
-
   private:
     /*!
      * \brief  Stores wallet information to wallet file.
